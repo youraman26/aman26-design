3be24c29e2a241c69e7b0d99b950c41617f1fb3e
youraman26 <chourasiyaaman76@gmail.com>
2026-05-25 23:59:53 +0530

    Updating the files

--- public/src/components/AnimatedHeading.tsx
+++
@@ -1,8 +0,0 @@
-import { motion } from "motion/react";
-import React from "react";
-
-export const AnimatedHeading = ({ text, className, style }: { text: React.ReactNode, className?: string, style?: React.CSSProperties }) => {
-  // If text is a string, we can split it. If it contains HTML like <br /> and <span>, it's harder.
-  // We can pass an array of words or a string, but the existing code has mixed content.
-  return null;
-}
